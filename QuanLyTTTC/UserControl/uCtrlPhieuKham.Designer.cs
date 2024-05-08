4a0e07449fb731bd09b9052c08cf02058a705c99
thieudz <c_ngthieu_02@outlook.com>
2024-05-08 08:40:34 +0700

    first commit

--- QuanLyTTTC/UserControl/uCtrlPhieuKham.Designer.cs
+++ QuanLyTTTC/UserControl/uCtrlPhieuKham.Designer.cs
@@ -28,18 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
             this.SuspendLayout();
+            // 
+            // guna2HtmlLabel1
+            // 
+            this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel1.Location = new System.Drawing.Point(419, 88);
+            this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            this.guna2HtmlLabel1.Size = new System.Drawing.Size(74, 18);
+            this.guna2HtmlLabel1.TabIndex = 0;
+            this.guna2HtmlLabel1.Text = "Phieu Kham";
             // 
             // uCtrlPhieuKham
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.guna2HtmlLabel1);
             this.Name = "uCtrlPhieuKham";
             this.Size = new System.Drawing.Size(1084, 629);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
     }
 }
